82c7d4838e5ec10c7d1c8baeb7fe60f7e5ef0fd0
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-07-18 19:14:00 -0400

    Some initial code. Can play one specific sound file, but will change that one can select a file from a specific folder.

---
+++ Soundboard/Soundboard/Soundboard.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Soundboard
+{
+    partial class frmSound
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lviewSounds = new System.Windows.Forms.ListView();
+            this.clmName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmLength = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cboxSoundDevices = new System.Windows.Forms.ComboBox();
+            this.lblDeviceSelection = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(31, 13);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lviewSounds
+            // 
+            this.lviewSounds.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmName,
+            this.clmLength});
+            this.lviewSounds.HideSelection = false;
+            this.lviewSounds.Location = new System.Drawing.Point(31, 42);
+            this.lviewSounds.Name = "lviewSounds";
+            this.lviewSounds.Size = new System.Drawing.Size(169, 202);
+            this.lviewSounds.TabIndex = 1;
+            this.lviewSounds.UseCompatibleStateImageBehavior = false;
+            this.lviewSounds.View = System.Windows.Forms.View.Details;
+            this.lviewSounds.SelectedIndexChanged += new System.EventHandler(this.lviewSounds_SelectedIndexChanged);
+            // 
+            // clmName
+            // 
+            this.clmName.Text = "Name";
+            // 
+            // clmLength
+            // 
+            this.clmLength.Text = "Length";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(125, 13);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // cboxSoundDevices
+            // 
+            this.cboxSoundDevices.FormattingEnabled = true;
+            this.cboxSoundDevices.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cboxSoundDevices.Location = new System.Drawing.Point(362, 58);
+            this.cboxSoundDevices.Name = "cboxSoundDevices";
+            this.cboxSoundDevices.Size = new System.Drawing.Size(299, 21);
+            this.cboxSoundDevices.TabIndex = 3;
+            // 
+            // lblDeviceSelection
+            // 
+            this.lblDeviceSelection.AutoSize = true;
+            this.lblDeviceSelection.Location = new System.Drawing.Point(463, 42);
+            this.lblDeviceSelection.Name = "lblDeviceSelection";
+            this.lblDeviceSelection.Size = new System.Drawing.Size(132, 13);
+            this.lblDeviceSelection.TabIndex = 4;
+            this.lblDeviceSelection.Text = "Output Device Selection : ";
+            // 
+            // frmSound
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDeviceSelection);
+            this.Controls.Add(this.cboxSoundDevices);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lviewSounds);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "frmSound";
+            this.Text = "Soundboard";
+            this.Load += new System.EventHandler(this.frmSound_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.ListView lviewSounds;
+        private System.Windows.Forms.ColumnHeader clmName;
+        private System.Windows.Forms.ColumnHeader clmLength;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ComboBox cboxSoundDevices;
+        private System.Windows.Forms.Label lblDeviceSelection;
+    }
+}
+
---
+++ Soundboard/Soundboard/Soundboard.cs
@@ -0,0 +1,67 @@
+﻿using NAudio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soundboard
+{
+    public partial class frmSound : Form
+    {
+        public frmSound()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            /*System.Media.SoundPlayer player = new System.Media.SoundPlayer("hellothere.mp3");
+            player.*/
+
+            //WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+            //player.URL = "hellothere.mp3";
+            //            player.playerApplication.
+            using (FileStream fs = new FileStream("hellothere.wav", FileMode.Open, FileAccess.Write, FileShare.ReadWrite))
+            {
+                var waveReader = new NAudio.Wave.WaveFileReader("hellothere1.wav");
+                var waveIn = new NAudio.Wave.WaveIn();
+                var waveOut = new NAudio.Wave.WaveOut();
+
+                waveIn.DeviceNumber = 0;
+                waveOut.DeviceNumber = cboxSoundDevices.SelectedIndex;
+                
+                var test = new NAudio.Wave.Mp3FileReader("hellothere.mp3");
+                waveOut.Init(test);
+                waveOut.Play();
+            }
+
+            
+
+            
+            //NAudio.Wave.DirectSoundOut.
+//            var device = new MMDeviceEnumerator();
+        }
+
+        private void lviewSounds_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmSound_Load(object sender, EventArgs e)
+        { 
+            //Load all of the sound devices
+            cboxSoundDevices.Items.Clear();
+            for (int deviceId = 0; deviceId < NAudio.Wave.WaveOut.DeviceCount; deviceId++)
+            {
+                var capabilities = NAudio.Wave.WaveOut.GetCapabilities(deviceId);
+                cboxSoundDevices.Items.Add(capabilities.ProductName);
+            }
+        }
+    }
+}
