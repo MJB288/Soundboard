72c460fc7344fa073c37ed1b013c389dd7c3633d
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-09-02 23:14:56 -0400

    Basic Class Outlines
    Added some basic classes for holding sound data. Basic outline on two, one has no code

---
+++ Soundboard/Soundboard/Classes/SoundFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard
+{
+    /// <summary>
+    /// A class that holds all of the necessary attributes for playback
+    /// </summary>
+    class SoundFile
+    {
+        public String soundName { get; set; }
+        public String filePath {get; set;}
+        public String groupName { get; set; }
+        public TimeSpan duration { get; set; }
+
+        /// <summary>
+        /// Constructs a SoundFile object. Automatically determines groupname from the last folder name in the filepath
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="path">The Filepath where the file is located</param>
+        /// <param name="duration">The duration of the sound file</param>
+        public SoundFile(String name, String path, TimeSpan duration)
+        {
+            soundName = name;
+            filePath = path;
+            this.duration = duration;
+            //The group name will automatically be determined by the lowest level folder holding the File
+            groupName = path.Split('\\')[path.Split('\\').Count()-2];
+        }
+
+        /// <summary>
+        /// Constructs a SoundFile Object. Allows User/Programmer input on what the group name should be
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="path"></param>
+        /// <param name="duration"></param>
+        /// <param name="groupName"></param>
+        public SoundFile(String name, String path, TimeSpan duration, String groupName)
+        {
+            soundName = name;
+            filePath = path;
+            this.duration = duration;
+        }
+    }
+}
---
+++ Soundboard/Soundboard/Classes/SoundFileFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace Soundboard.Classes
+{
+    /// <summary>
+    /// A factory pattern object designed for constructing SoundFile objects
+    /// </summary>
+    class SoundFileFactory
+    {
+        public string[] FilePaths { get; set; }
+        public SoundFileFactory(string[] filePaths)
+        {
+            FilePaths = filePaths;
+        }
+
+        public Dictionary<String, List<SoundFile>> constructSoundFiles()
+        {
+            //First off check for null or empty data - don't run this if true
+            if (FilePaths == null || FilePaths.Count() == 0) return null;
+            Dictionary<String, List<SoundFile>> soundData = new Dictionary<string, List<SoundFile>>();
+            //Now iterate through each filepath
+            foreach (String filePath in FilePaths)
+            {
+                
+            }
+            return soundData;
+        }
+    }
+}
