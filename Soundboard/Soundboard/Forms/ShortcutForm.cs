2f591ac881a26f9d9fd1aa4b621f685d91a30eb6
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-11-30 23:53:56 -0500

    KeyPress Up, and Exclude Keys
    Switched to the KeyUp event from the KeyPress event. Added a static array of key presses that will not be recognized as inividual keys (shift, control, alt).

--- Soundboard/Soundboard/Forms/ShortcutForm.cs
+++ Soundboard/Soundboard/Forms/ShortcutForm.cs
@@ -12,6 +12,10 @@ namespace Soundboard.Forms
 {
     public partial class frmShortcutForm : Form
     {
+        /// <summary>
+        /// A static array of Key Codes that should not be recognized as individual key presses for shortcuts
+        /// </summary>
+        public static Keys[] NoShortcutAlone = { Keys.ShiftKey, Keys.ControlKey, Keys.Alt, Keys.Menu};
         public frmShortcutForm()
         {
             InitializeComponent();
@@ -19,12 +23,43 @@ namespace Soundboard.Forms
 
         private void frmShortcutForm_Load(object sender, EventArgs e)
         {
-            this.KeyPress += getPressedKey;
+            //this.KeyPress += getPressedKey;
+            this.KeyUp += getPressedKeyUp;
         }
 
         private void getPressedKey(object sender, KeyPressEventArgs ke)
         {
             lblDisplayKey.Text = "Pressed Key = " + ke.KeyChar;
+            if (Form.ModifierKeys.HasFlag(Keys.Shift))
+            {
+                lblDisplayKey.Text += " Shift";
+            }
+        }
+
+        private void getPressedKeyUp(object sender, KeyEventArgs ke)
+        {
+            //I don't want Shift, Ctrl, or Alt to be registered as individual keypresses
+            //Also - 
+            if (!NoShortcutAlone.Contains(ke.KeyCode))
+            {
+                String keyCombo = "";
+                if (Form.ModifierKeys.HasFlag(Keys.Control))
+                {
+                   keyCombo += "Ctrl ";
+                }
+                if (Form.ModifierKeys.HasFlag(Keys.Shift))
+                {
+                    keyCombo += "Shift ";
+                }
+                /*
+                if(Form.ModifierKeys.HasFlag(Keys.Alt))
+                {
+                    keyCombo += "Alt ";
+                }*/
+                keyCombo += ke.KeyCode.ToString();
+                lblDisplayKey.Text = "Pressed Key = " + keyCombo;
+                
+            }
         }
     }
 }
